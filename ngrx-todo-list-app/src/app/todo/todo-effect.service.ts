bf67b376bf71f45125d47872eedf1337969521a2
Frédéric GAURAT <fgaurat@gmail.com>
2025-01-30 16:49:45 +0100

    add effect

--- ngrx-todo-list-app/src/app/todo/todo-effect.service.ts
+++ ngrx-todo-list-app/src/app/todo/todo-effect.service.ts
@@ -1,6 +1,9 @@
 import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { TodoService } from '../services/todo.service';
+import { loadTodos, loadTodosSuccess } from './todo.actions';
+import { map, switchMap } from 'rxjs';
+import { Todos } from '../models/todo';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +13,10 @@ export class TodoEffectService {
   private todoService = inject(TodoService);
 
   loadTodos$ = createEffect(() => {
-    ofType()
+    return this.actions$.pipe(
+      ofType(loadTodos),
+      switchMap(() => this.todoService.findAll()),
+      map((todos: Todos) => loadTodosSuccess({ todos }))
+    )
   })
 }
--- ngrx-todo-list-app/src/app/todo/todo.actions.ts
+++ ngrx-todo-list-app/src/app/todo/todo.actions.ts
@@ -1,6 +1,7 @@
-import { createAction } from "@ngrx/store";
+import { createAction, props } from "@ngrx/store";
 import { ActionType } from "../models/action-type";
+import { Todos } from "../models/todo";
 
 
 export const loadTodos = createAction(ActionType.LOAD_TODOS)
-export const loadTodosSuccess = createAction(ActionType.LOAD_TODOS_SUCCESS)
+export const loadTodosSuccess = createAction(ActionType.LOAD_TODOS_SUCCESS,props<{todos:Todos}>())
